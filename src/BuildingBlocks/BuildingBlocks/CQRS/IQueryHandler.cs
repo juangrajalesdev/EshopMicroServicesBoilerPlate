4b35ecd0a1ae57ad2bf576bd8c35bda7963fe150
Juan Grajales <devjuangiraldo@gmail.com>
2026-02-28 21:13:21 -0500

    MediatR Abstraction

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.CQRS
+{
+    internal interface IQueryHandler <in Tquery,TResponse>
+        :IRequestHandler<Tquery,TResponse>
+        where Tquery : IQuery<TResponse>
+        where TResponse : notnull 
+    {
+    }
+}
