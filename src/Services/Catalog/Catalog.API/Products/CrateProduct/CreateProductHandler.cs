38a0694f0f1943710903a1baf124377b82e0250d
Juan Grajales <devjuangiraldo@gmail.com>
2026-02-28 20:41:52 -0500

    add ProductHandler, and init Building Blocks

---
+++ src/Services/Catalog/Catalog.API/Products/CrateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CrateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id); 
+    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+                throw new NotImplementedException();
+        }
+
+    }
+}
