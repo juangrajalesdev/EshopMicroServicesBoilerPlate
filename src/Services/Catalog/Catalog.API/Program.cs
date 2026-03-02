66c6fb2407e197cd91cc78f2b29578393fcf00a7
Juan Grajales <devjuangiraldo@gmail.com>
2026-03-01 21:58:01 -0500

    add libraries on Program

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,8 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
