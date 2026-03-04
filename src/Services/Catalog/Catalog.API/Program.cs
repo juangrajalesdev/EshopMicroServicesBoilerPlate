88193d759f2ef9cdcd7860ad36ffe6905a41f3b3
Juan Grajales <devjuangiraldo@gmail.com>
2026-03-03 22:43:47 -0500

    added
    docker compose
    marten on program.cs

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -3,6 +3,10 @@ using Carter;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("DataBase"));
+}).UseLightweightSessions();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
