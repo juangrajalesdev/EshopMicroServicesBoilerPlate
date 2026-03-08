2c3ac87929c2b9078a9e3a276af14a6fcf206c6a
Juan Grajales <devjuangiraldo@gmail.com>
2026-03-07 21:44:44 -0500

    add GetEndPoint and Handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsEndPoint.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Products.GetProduct;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsController : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+
+                var response = result.Adapt<GetProductsResponse>();
+                
+                return Results.Ok(response);
+            });
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsHandler.cs
@@ -0,0 +1,23 @@
+﻿using Marten.Linq.QueryHandlers;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace Catalog.API.Products.GetProduct
+{
+
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler
+        (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IRequestHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Getting all products");
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
