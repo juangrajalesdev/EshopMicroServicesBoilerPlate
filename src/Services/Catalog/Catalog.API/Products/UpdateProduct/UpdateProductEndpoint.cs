1cd2a8cd5ba6610542efe964777bc7623fc1dfec
Juan Grajales <devjuangiraldo@gmail.com>
2026-03-08 21:26:10 -0500

    Add Update Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductRequest(Guid Id,string Name, List<string> Categories,string Description,string ImageFile,decimal Price);
+    public record UpdateProductResponse(bool isSuccess);
+    public class UpdateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request , ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+
+                return Results.Ok(result.Adapt<UpdateProductResponse>());
+            })
+            .WithName("UpdateProduct")
+            .WithTags("Products")
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Update Product")
+            .WithDescription("Updates a product by its unique identifier.");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid id,string Name, List<string> Categories,string Description,string ImageFile,decimal Price) : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool isSuccess);
+
+    internal class UpdateProductQueryHandler
+        (IDocumentSession session, ILogger<UpdateProductQueryHandler> logger)
+        : IRequestHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = await session.LoadAsync<Product>(command.id, cancellationToken);
+            if (product is null)
+            {
+                logger.LogWarning("Product with id {id} not found", command.id);
+                throw new ProductNotFoundException(command.id);
+            }
+            product.Name = command.Name;
+            product.Category = command.Categories;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+            
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new UpdateProductResult(true);
+        }   
+
+    }
+}
