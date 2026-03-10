e84c2d00c0006fefe94ae3834c7b328af78a66d2
Juan Grajales <devjuangiraldo@gmail.com>
2026-03-09 20:45:53 -0500

    Add DeleteProduct Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProductById/DeleteProductByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.DeleteProductById
+{
+    public record DeleteProductByIdRequest(Guid Id);
+    public record DeleteProductResponse(Guid Id);
+    public class DeleteProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+                
+                var response = result.Adapt<DeleteProductResponse>();
+
+                return Results.NoContent();
+            })
+            .WithName("DeleteProductById")
+            .WithTags("Products")
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Product By Id")
+            .WithDescription("Deletes a product by its unique identifier.");
+        }
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProductById/DeleteProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Products.GetProduct;
+
+namespace Catalog.API.Products.DeleteProductById
+{
+    public record DeleteProductCommand(Guid id) : ICommand<DeleteProductByIdResult>;
+    public record DeleteProductByIdResult(bool isSuccess);
+    public class DeleteProductByIdQueryHandler
+        (IDocumentSession session,ILogger<DeleteProductByIdQueryHandler> logger)
+        : ICommandHandler<DeleteProductCommand, DeleteProductByIdResult>
+
+    {
+        public async Task <DeleteProductByIdResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductByIdQueryHandler called with command: {@command}", command);
+
+            session.Delete<Product>(command.id);
+            await session.SaveChangesAsync();
+
+            return new DeleteProductByIdResult(true);
+        }
+
+    }
+}
